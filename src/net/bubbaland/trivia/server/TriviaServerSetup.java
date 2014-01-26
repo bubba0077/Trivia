1a64c105b6480e86b0ae3a629d12c3936885245e
bubba0077 <wckolczy@alum.wustl.edu>
2014-01-26 11:20:46 -0800

    Added no-GUI option to the server setup program.

--- src/net/bubbaland/trivia/server/TriviaServerSetup.java
+++ src/net/bubbaland/trivia/server/TriviaServerSetup.java
@@ -1,12 +1,14 @@
 package net.bubbaland.trivia.server;
 
 import java.awt.BorderLayout;
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.FileChannel;
@@ -39,44 +41,122 @@ public class TriviaServerSetup {
 	private static String		SOURCE_URL			= "http://www.bubbaland.net/trivia";
 	private static String[]		FILENAMES			= { "triviaClient.jar", "triviaServer.jar",
 			"lib/jcommon-1.0.20.jar", "lib/jfreechart-1.0.16.jar", "lib/jfxrt.jar" };
+	private static boolean		showGUI;
+
+	private static final String	welcomeMsg			= "This program will download all of the files necessary to host the trivia server and configure the necessary settings.\n\n"
+															+ "Before continuing, please make sure you have all of the following required components set up:\n"
+															+ "1) An internet-facing html server for the webstart files\n"
+															+ "2) Another available internet-facing port for the trivia server (default is 1099)";
+
+	private static final String	requestHTML			= "Enter local html root directory. A folder named trivia will be created there which will serve the trivia files.";
+	private static final String	defaultHTML			= "/var/www";
+
+	private static final String	requestURL			= "Enter domain name where the jars and java webstart files will be hosted. Do not include a protocol.";
+	private static final String	defaultURL			= "www.bubbaland.net";
+
+	private static final String	requestPort			= "Enter port to use for server. The port must be accept connections from the internet.";
+	private static final int	defaultPort			= 1099;
 
 	public static void main(String args[]) {
 
-		final JFrame frame = new JFrame("Trivia Server Setup");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		JTextArea textArea = new JTextArea();
-		textArea.setEditable(false);
-		textArea.setLineWrap(true);
-		textArea.setWrapStyleWord(true);
-		textArea.setAutoscrolls(true);
-		final DefaultCaret caret = (DefaultCaret) textArea.getCaret();
-		caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
-		JScrollPane pane = new JScrollPane(textArea);
-		frame.getContentPane().add(pane, BorderLayout.CENTER);
-		frame.setSize(400, 200);
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
+		if (args.length > 0 && args[0].equals("noGUI")) {
+			showGUI = false;
+		} else {
+			showGUI = true;
+		}
 
-		JOptionPane
-				.showMessageDialog(
-						frame,
-						"This program will download all of the files necessary to host the trivia server and configure the necessary settings.\n\n"
-								+ "Before continuing, please make sure you have all of the following required components set up:\n"
-								+ "1) An internet-facing html server for the webstart files\n"
-								+ "2) Another available internet-facing port for the trivia server (default is 1099)");
+		JFrame frame = null;
+		JTextArea textArea = null;
+		InputStreamReader istream = new InputStreamReader(System.in);
+		BufferedReader bufRead = new BufferedReader(istream);
+
+		if (showGUI) {
+			frame = new JFrame("Trivia Server Setup");
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			textArea = new JTextArea();
+			textArea.setEditable(false);
+			textArea.setLineWrap(true);
+			textArea.setWrapStyleWord(true);
+			textArea.setAutoscrolls(true);
+			final DefaultCaret caret = (DefaultCaret) textArea.getCaret();
+			caret.setUpdatePolicy(DefaultCaret.ALWAYS_UPDATE);
+			JScrollPane pane = new JScrollPane(textArea);
+			frame.getContentPane().add(pane, BorderLayout.CENTER);
+			frame.setSize(400, 200);
+			frame.setLocationRelativeTo(null);
+			frame.setVisible(true);
+
+			JOptionPane.showMessageDialog(frame, welcomeMsg);
+		} else {
+			System.out.println(welcomeMsg);
+		}
 
 		/*
 		 * Ask for local html root directory
 		 */
 		String htmlDirName = null;
 		while (htmlDirName == null) {
-			textArea.append("Requesting location of html root directory\n");
-			htmlDirName = (String) JOptionPane
-					.showInputDialog(
-							frame,
-							"Enter local html root directory. A folder named trivia will be created there which will serve the trivia files.",
-							"Trivia Server Setup", JOptionPane.PLAIN_MESSAGE, null, null, "/www/var");
+			if (showGUI) {
+				textArea.append("Requesting location of html root directory\n");
+				htmlDirName = (String) JOptionPane.showInputDialog(frame, requestHTML, "Trivia Server Setup",
+						JOptionPane.PLAIN_MESSAGE, null, null, defaultHTML);
+			} else {
+				System.out.println(requestHTML + " [" + defaultHTML + "]");
+				try {
+					htmlDirName = bufRead.readLine();
+				} catch (IOException exception) {
+					System.out.println("Error reading input stream, exiting");
+					System.exit(1);
+				}
+				if (htmlDirName.equals("")) {
+					System.out.println("Using default value: " + defaultHTML);
+					htmlDirName = defaultHTML;
+				}
+			}
 			if (htmlDirName == null) {
+				if (showGUI) {
+					textArea.append("Setup cancelled, exiting!\n");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException exception) {
+					}
+					System.exit(0);
+				}
+			}
+			if (!new File(htmlDirName).isDirectory()) {
+				htmlDirName = null;
+				if (showGUI) {
+					JOptionPane.showMessageDialog(frame, "Directory does not exist, please try again.");
+					textArea.append("Invalid directory specified, repeating request.\n");
+				} else {
+					System.out.println("Directory does not exist, please try again.");
+				}
+			}
+		}
+
+		/*
+		 * Ask for the server URL
+		 */
+		String serverURL = null;
+		if (showGUI) {
+			textArea.append("Requesting server URL\n");
+			serverURL = (String) JOptionPane.showInputDialog(frame, requestURL, "Trivia Server Setup",
+					JOptionPane.PLAIN_MESSAGE, null, null, defaultURL);
+		} else {
+			System.out.println(requestURL + " [" + defaultURL + "]");
+			try {
+				serverURL = bufRead.readLine();
+			} catch (IOException exception) {
+				System.out.println("Error reading input stream, exiting");
+				System.exit(1);
+			}
+			if (serverURL.equals("")) {
+				System.out.println("Using default: " + defaultURL);
+				serverURL = defaultURL;
+			}
+		}
+		if (serverURL == null) {
+			if (showGUI) {
 				textArea.append("Setup cancelled, exiting!\n");
 				try {
 					Thread.sleep(1000);
@@ -84,27 +164,6 @@ public class TriviaServerSetup {
 				}
 				System.exit(0);
 			}
-			if (!new File(htmlDirName).isDirectory()) {
-				htmlDirName = null;
-				JOptionPane.showMessageDialog(frame, "Directory does not exist, please try again.");
-				textArea.append("Invalid directory specified, repeating request.\n");
-			}
-		}
-
-		/*
-		 * Ask for the server URL
-		 */
-		textArea.append("Requesting server URL\n");
-		final String serverURL = (String) JOptionPane.showInputDialog(frame,
-				"Enter domain name where the jars and java webstart files will be hosted. Do not include a protocol.",
-				"Trivia Server Setup", JOptionPane.PLAIN_MESSAGE, null, null, "www.bubbaland.net");
-		if (serverURL == null) {
-			textArea.append("Setup cancelled, exiting!\n");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException exception) {
-			}
-			System.exit(0);
 		}
 
 		/*
@@ -113,23 +172,41 @@ public class TriviaServerSetup {
 		int port = 0;
 		while (port == 0) {
 			try {
-				port = Integer.parseInt((String) JOptionPane.showInputDialog(frame,
-						"Enter port to use for server. The port must be accept connections from the internet.",
-						"Trivia Server Setup", JOptionPane.PLAIN_MESSAGE, null, null, "1099"));
+				if (showGUI) {
+					port = Integer.parseInt((String) JOptionPane.showInputDialog(frame, requestPort,
+							"Trivia Server Setup", JOptionPane.PLAIN_MESSAGE, null, null, defaultPort));
+				} else {
+					System.out.println(requestPort + " [" + defaultPort + "]");
+					final String portString = bufRead.readLine();
+					if (portString.equals("")) {
+						port = defaultPort;
+					} else {
+						port = Integer.parseInt(portString);
+					}
+				}
 			} catch (NumberFormatException exception) {
 				port = 0;
 			} catch (NullPointerException exception) {
-				textArea.append("Setup cancelled, exiting!\n");
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException exception1) {
+				if (showGUI) {
+					textArea.append("Setup cancelled, exiting!\n");
+					try {
+						Thread.sleep(1000);
+					} catch (InterruptedException exception1) {
+					}
+					System.exit(0);
 				}
-				System.exit(0);
+			} catch (IOException exception) {
+				System.out.println("Error reading input stream, exiting");
+				System.exit(1);
 			}
 			if (port < 1 || port > 65535) {
 				port = 0;
-				JOptionPane.showMessageDialog(frame, "Port must be an integer between 1 and 65535");
-				textArea.append("Invalid port number input, trying again.\n");
+				if (showGUI) {
+					JOptionPane.showMessageDialog(frame, "Port must be an integer between 1 and 65535");
+					textArea.append("Invalid port number input, trying again.\n");
+				} else {
+					System.out.println("Port must be an integer between 1 and 65535");
+				}
 			}
 		}
 
@@ -143,19 +220,35 @@ public class TriviaServerSetup {
 		final File libDir = new File(triviaDirPath + "/lib");
 
 		if (!triviaDir.isDirectory()) {
-			textArea.append("Creating " + triviaDirPath + "\n");
+			if (showGUI) {
+				textArea.append("Creating " + triviaDirPath + "\n");
+			} else {
+				System.out.println("Creating " + triviaDirPath + "\n");
+			}
 			triviaDir.mkdir();
 		}
 		if (!savesDir.isDirectory()) {
-			textArea.append("Creating " + triviaDirPath + "/saves\n");
+			if (showGUI) {
+				textArea.append("Creating " + triviaDirPath + "/saves\n");
+			} else {
+				System.out.println("Creating " + triviaDirPath + "/saves\n");
+			}
 			savesDir.mkdir();
 		}
 		if (!chartDir.isDirectory()) {
-			textArea.append("Creating " + triviaDirPath + "/charts\n");
+			if (showGUI) {
+				textArea.append("Creating " + triviaDirPath + "/charts\n");
+			} else {
+				System.out.println("Creating " + triviaDirPath + "/charts\n");
+			}
 			chartDir.mkdir();
 		}
 		if (!libDir.isDirectory()) {
-			textArea.append("Creating " + triviaDirPath + "/lib\n");
+			if (showGUI) {
+				textArea.append("Creating " + triviaDirPath + "/lib\n");
+			} else {
+				System.out.println("Creating " + triviaDirPath + "/lib\n");
+			}
 			libDir.mkdir();
 		}
 
@@ -166,7 +259,11 @@ public class TriviaServerSetup {
 		for (String filename : FILENAMES) {
 			final File file = new File(triviaDirPath + "/" + filename);
 			if (!file.exists()) {
-				textArea.append("Downloading " + filename + "\n");
+				if (showGUI) {
+					textArea.append("Downloading " + filename + "\n");
+				} else {
+					System.out.println("Downloading " + filename + "\n");
+				}
 				downloadFile(filename, triviaDirPath, textArea);
 			}
 		}
@@ -174,22 +271,42 @@ public class TriviaServerSetup {
 		/*
 		 * Create webstart files
 		 */
-		textArea.append("Creating triviaClient.jnlp\n");
+		if (showGUI) {
+			textArea.append("Creating triviaClient.jnlp\n");
+		} else {
+			System.out.println("Creating triviaClient.jnlp\n");
+		}
 		createJNLP(triviaDirPath + "/triviaClient.jnlp", serverURL, port, textArea);
-		textArea.append("Creating triviaClient_IRC.jnlp\n");
+		if (showGUI) {
+			textArea.append("Creating triviaClient_IRC.jnlp\n");
+		} else {
+			System.out.println("Creating triviaClient_IRC.jnlp\n");
+		}
 		createJNLP(triviaDirPath + "/triviaClient_IRC.jnlp", serverURL, port, true, textArea);
 
 		/*
 		 * Create server settings file
 		 */
-		textArea.append("Creating " + System.getProperty("user.home") + "/" + SETTINGS_FILENAME + "\n\n");
+		if (showGUI) {
+			textArea.append("Creating " + System.getProperty("user.home") + "/" + SETTINGS_FILENAME + "\n\n");
+		} else {
+			System.out.println("Creating " + System.getProperty("user.home") + "/" + SETTINGS_FILENAME + "\n\n");
+		}
 		createServerSettings(triviaDirPath, serverURL, port);
 
-		textArea.append("Your trivia server is now set up. You can run it with the following command:\n");
-		textArea.append(">java -jar " + triviaDirPath + "/triviaServer.jar\n\n");
-		textArea.append("Users can access the client through the following links:\n");
-		textArea.append("http://" + serverURL + "/trivia/triviaClient.jnlp\n");
-		textArea.append("http://" + serverURL + "/trivia/triviaClient_IRC.jnlp");
+		if (showGUI) {
+			textArea.append("Your trivia server is now set up. You can run it with the following command:\n");
+			textArea.append(">java -jar " + triviaDirPath + "/triviaServer.jar\n\n");
+			textArea.append("Users can access the client through the following links:\n");
+			textArea.append("http://" + serverURL + "/trivia/triviaClient.jnlp\n");
+			textArea.append("http://" + serverURL + "/trivia/triviaClient_IRC.jnlp");
+		} else {
+			System.out.println("Your trivia server is now set up. You can run it with the following command:\n");
+			System.out.println(">java -jar " + triviaDirPath + "/triviaServer.jar\n\n");
+			System.out.println("Users can access the client through the following links:\n");
+			System.out.println("http://" + serverURL + "/trivia/triviaClient.jnlp\n");
+			System.out.println("http://" + serverURL + "/trivia/triviaClient_IRC.jnlp");
+		}
 	}
 
 	private static void downloadFile(String filename, String dir, JTextArea textArea) {
