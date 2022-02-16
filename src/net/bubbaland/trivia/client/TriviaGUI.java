78c718f45fe25102a6740e79296e26db43f0dbb7
Walter Kolczynski <Walter.Kolczynski@gmail.com>
2022-02-15 23:02:23 -0500

    Updates for Trivia 2022
    Basic updates for Trivia 2020:
    - Visual Trivia base URL updated
    - Number of rounds restored to 50

--- src/net/bubbaland/trivia/client/TriviaGUI.java
+++ src/net/bubbaland/trivia/client/TriviaGUI.java
@@ -66,7 +66,7 @@ public class TriviaGUI implements WindowListener {
 	final static protected String	KVSC_URL					= "http://www.kvsc.org/trivia_news.php";
 	// URL base for Visual Trivia Pages
 	final static protected String	VISUAL_URL					=
-			"https://sites.google.com/a/kneedeepintheses.org/information/trivia-2021/visual-trivia-2021/visual-trivia-";
+			"https://sites.google.com/a/kneedeepintheses.org/information/trivia-2022/visual-trivia-2022/visual-trivia-";
 	// URL for audio trivia
 	// final static protected String AUDIO_URL = "http://trivia.bgsh.org/audio";
 	final static protected String	AUDIO_URL					= "http://trivia.bgsh.org/data/audio";
