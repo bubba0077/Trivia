12f57991031e3f1c2f0250d935227b2ae699982c
Walter Kolczynski <walter.kolczynski@gmail.com>
2024-02-15 21:54:30 -0500

    Make signjars more maintainable
    Change-Id: I8226c67da96e37a4d88446106c5656b8b205ba29

--- scripts/signjars.sh
+++ scripts/signjars.sh
@@ -1,3 +1,9 @@
 #! /usr/bin/env bash
-jarsigner /mnt/c/Apache/htdocs/trivia/triviaClient.jar -keystore keystore.jks -tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp "Walter Kolczynski"
-jarsigner /mnt/c/Users/Walter\ Kolczynski/trivia_run/triviaServer.jar -keystore keystore.jks -tsa http://sha256timestamp.ws.symantec.com/sha256/timestamp "Walter Kolczynski"
+
+client_location="/mnt/hgfs/C/Apache/htdocs/trivia/triviaClient.jar"
+server_location="/mnt/hgfs/C/Users/Walter/trivia_run/scripts/triviaServer.jar"
+keystore="keystore.jks"
+time_server="http://sha256timestamp.ws.symantec.com/sha256/timestamp"
+
+jarsigner ${client_location} -keystore ${keystore} -tsa ${time_server} "Walter Kolczynski"
+jarsigner ${server_location} -keystore ${keystore} -tsa ${time_server} "Walter Kolczynski"
